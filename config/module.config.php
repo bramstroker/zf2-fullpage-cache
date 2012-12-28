8b43fca861c0c22a0822227fc94e9cc50960b790
bramstroker <bgerritsen@gmail.com>
2012-12-28 21:53:49 +0100

    use default cachedir

--- config/module.config.php
+++ config/module.config.php
@@ -3,9 +3,6 @@ return array(
     'strokercache' => array(
         'storage_adapter' => array(
             'name' => 'Zend\Cache\Storage\Adapter\FileSystem',
-            'options' => array(
-                'cache_dir' => __DIR__ . '/../../../data'
-            )
         ),
         'strategies' => array(
             'plugin_manager' => array(

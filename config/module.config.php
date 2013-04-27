bf65b04df41a5b5e0477c525204a5af153683aa2
Ioan Katalin <catalin_web@yahoo.com>
2013-04-27 20:33:58 +0300

    Making it work with zf2 2.1.6 dev
    Making it work with zf2 2.1.6 dev

--- config/module.config.php
+++ config/module.config.php
@@ -37,7 +37,7 @@ return array(
 
     'strokercache' => array(
         'storage_adapter' => array(
-            'name' => 'Zend\Cache\Storage\Adapter\FileSystem',
+            'name' => 'Zend\Cache\Storage\Adapter\Filesystem',
         ),
         'strategies' => array(
             'plugin_manager' => array(

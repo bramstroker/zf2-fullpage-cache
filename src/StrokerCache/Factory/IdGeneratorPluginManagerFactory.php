5458c4b5993b1bc327142d7211dece4d3fbb49a8
arekkas <aeneas.rekkas@serlo.org>
2014-06-15 23:52:12 +0200

    Fixed docblocks

--- src/StrokerCache/Factory/IdGeneratorPluginManagerFactory.php
+++ src/StrokerCache/Factory/IdGeneratorPluginManagerFactory.php
@@ -1,13 +1,11 @@
 <?php
 /**
- * Athene2 - Advanced Learning Resources Manager
- *
- * @author    Aeneas Rekkas (aeneas.rekkas@serlo.org)
- * @license   LGPL-3.0
- * @license   http://opensource.org/licenses/LGPL-3.0 The GNU Lesser General Public License, version 3.0
- * @link      https://github.com/serlo-org/athene2 for the canonical source repository
- * @copyright Copyright (c) 2013-2014 Gesellschaft für freie Bildung e.V. (http://www.open-education.eu/)
+ * @author Aeneas Rekkas
+ * @copyright (c) Aeneas Rekkas 2014
+ * @license http://opensource.org/licenses/mit-license.php
  */
+
+
 namespace StrokerCache\Factory;
 
 use StrokerCache\IdGenerator\IdGeneratorPluginManager;
@@ -18,10 +16,7 @@ use Zend\ServiceManager\ServiceLocatorInterface;
 class IdGeneratorPluginManagerFactory implements FactoryInterface
 {
     /**
-     * Create service
-     *
-     * @param ServiceLocatorInterface $serviceLocator
-     * @return mixed
+     * {@inheritDoc}
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {

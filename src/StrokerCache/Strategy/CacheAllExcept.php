64c98140fffa34043eee7f24108b35c448498254
Jonathan Maron <jonathan.maron@theimagingsource.com>
2016-02-18 13:35:49 +0100

    In the case of a non-matched route,  should be false

--- src/StrokerCache/Strategy/CacheAllExcept.php
+++ src/StrokerCache/Strategy/CacheAllExcept.php
@@ -8,8 +8,12 @@
 namespace StrokerCache\Strategy;
 
 use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\Http\RouteMatch    as RouteMatchHttp;
+use Zend\Mvc\Router\Console\RouteMatch as RouteMatchConsole;
 
 use StrokerCache\Exception\BadConfigurationException;
+
+
 
 class CacheAllExcept extends AbstractStrategy
 {
@@ -27,48 +31,54 @@ class CacheAllExcept extends AbstractStrategy
 
         if (!isset($except['namespaces']) && !isset($except['controllers']) && !isset($except['actions'])) {
             throw new BadConfigurationException(
-                "At least one of ['namespaces', 'controllers', 'actions'] keys has to be set in the "
+                  "At least one of ['namespaces', 'controllers', 'actions'] keys has to be set in the "
                 . "\$config['strokercache']['strategies']['enabled']['" . __CLASS__ . "']['except'][] "
                 . "confiuration array."
             );
         }
 
-        $routeMatch  = $event->getRouteMatch();
+        $shouldCache = false;
 
-        $controller  = $this->normalize($routeMatch->getParam('controller'));
-        $action      = $this->normalize($routeMatch->getParam('action'));
+        $routeMatch = $event->getRouteMatch();
 
-        $shouldCache = true;
+        if ($routeMatch instanceof RouteMatchHttp || $routeMatch instanceof RouteMatchConsole) {
 
-        if (true === $shouldCache && isset($except['namespaces'])) {
-            foreach ($except['namespaces'] as $exceptNamespace) {
-                if (0 === strpos($controller, $this->normalize($exceptNamespace))) {
-                    $shouldCache = false;
-                    break 1;
+            $shouldCache = true;
+
+            $controller  = $this->normalize($routeMatch->getParam('controller'));
+            $action      = $this->normalize($routeMatch->getParam('action'));
+
+            if (true === $shouldCache && isset($except['namespaces'])) {
+                foreach ($except['namespaces'] as $exceptNamespace) {
+                    if (0 === strpos($controller, $this->normalize($exceptNamespace))) {
+                        $shouldCache = false;
+                        break 1;
+                    }
                 }
             }
-        }
 
-        if (true === $shouldCache && isset($except['controllers'])) {
-            foreach ($except['controllers'] as $exceptController) {
-                if ($controller === $this->normalize($exceptController)) {
-                    $shouldCache = false;
-                    break 1;
+            if (true === $shouldCache && isset($except['controllers'])) {
+                foreach ($except['controllers'] as $exceptController) {
+                    if ($controller === $this->normalize($exceptController)) {
+                        $shouldCache = false;
+                        break 1;
+                    }
                 }
             }
-        }
 
-        if (true === $shouldCache && isset($except['actions'])) {
-            foreach ($except['actions'] as $exceptController => $exceptActions) {
-                if ($controller === $this->normalize($exceptController)) {
-                    foreach ($exceptActions as $exceptAction) {
-                        if ($action === $this->normalize($exceptAction)) {
-                            $shouldCache = false;
-                            break 2;
+            if (true === $shouldCache && isset($except['actions'])) {
+                foreach ($except['actions'] as $exceptController => $exceptActions) {
+                    if ($controller === $this->normalize($exceptController)) {
+                        foreach ($exceptActions as $exceptAction) {
+                            if ($action === $this->normalize($exceptAction)) {
+                                $shouldCache = false;
+                                break 2;
+                            }
                         }
                     }
                 }
             }
+
         }
 
         return $shouldCache;
